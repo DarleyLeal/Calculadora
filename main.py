85a2eba7d105aee25a05eba54f05edb3c898dde2
Darley Leal <darleyleal98@gmail.com>
2022-09-10 08:46:20 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+preco = float(input('Digite o preço do produto:'))
+p = float(input('Digite o percentual de desconto (0-100)%'))
+
+desconto = preco * (p / 100)
+final = preco - desconto
+
+print('0 o preço do produto é {}, desconto de {}%' .format(preco,p))
+print('Valor calculado de desconto: {}. Valor final do produto: '.format(desconto, final))
